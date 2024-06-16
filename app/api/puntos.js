8060176038c8ce427e5178ae8373a35bd9012d32
felipe pinuer <pinuer15@gmail.com>
2024-06-16 15:39:20 -0400

    Se actualiza el puntos.js

--- app/api/puntos.js
+++ app/api/puntos.js
@@ -3,10 +3,12 @@ import { NextResponse } from "next/server";
 export const puntos =[
     {id: 1, nombre : "La Cervecería Kunstmann ", coordenadas : [-39.836583, -73.278583], 
     tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}],
-    linkD:"https://www.google.com/maps/place/%22La+Cervecer%C3%ADa%22+Kunstmann/@-39.8368501,-73.2808802,17z/data=!3m1!4b1!4m6!3m5!1s0x9615f1e07207c325:0xe36903a999717487!8m2!3d-39.8368501!4d-73.2783053!16s%2Fg%2F11b7q5fnmy?entry=ttu"},
+    linkD:"https://www.google.com/maps/place/%22La+Cervecer%C3%ADa%22+Kunstmann/@-39.8368501,-73.2808802,17z/data=!3m1!4b1!4m6!3m5!1s0x9615f1e07207c325:0xe36903a999717487!8m2!3d-39.8368501!4d-73.2783053!16s%2Fg%2F11b7q5fnmy?entry=ttu",
+    servcio:["Consumo en el lugar", "Para llevar", "Entrega"]},
     
     {id:2, nombre:"Cerveceria Nothus", coordenadas: [-39.824107, -73.238481],
-    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 0}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 0}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}], 
+    linkD:"https://www.google.com/maps/place/Cervecer%C3%ADa+Nothus/@-39.8242023,-73.2410789,17z/data=!3m1!4b1!4m6!3m5!1s0x9615ef0e551fe46f:0xfa8f800ffdf30f2c!8m2!3d-39.8242023!4d-73.238504!16s%2Fg%2F11f7n4qfrm?entry=ttu"},
 
     
     {id:3, nombre:"Cerveceria Calle Calle", coordenadas: [-39.855059, -73.189059],
