cdcd98d8ac2416d70ed550fbb9118c19e4cb29ca
felipe pinuer <pinuer15@gmail.com>
2024-06-16 15:15:11 -0400

    Se actualiza el puntos.js

--- app/api/puntos.js
+++ app/api/puntos.js
@@ -13,8 +13,42 @@ export const puntos =[
     tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
 
     {id:4, nombre:"Cerveceria los Torreones", coordenadas: [-39.807622621037474, -73.21547458765774],
-    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]}
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:5, nombre:"Bar Bundor", coordenadas: [-39.80899418365889, -73.25492250642007],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:6, nombre:"El Growler", coordenadas: [-39.8097984810652, -73.25409887445188],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:7, nombre:"El Regreso Beer Company", coordenadas: [-39.819660127756805, -73.24644226089781],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:8, nombre:"Cerveza carva", coordenadas: [-39.834844724481535, -73.2187401575215],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:9, nombre:"Cerveza Totem", coordenadas: [-39.85027684666218, -73.19666409984868],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:10, nombre:"rakkar Hidromiel", coordenadas: [-39.82687211885121, -73.22671976266108],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:11, nombre:"Cerveza Schenke Brüder", coordenadas: [-39.82687211885121, -73.22671976266108],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:12, nombre:"Cerveza 3 Puentes", coordenadas: [-39.82687211885121, -73.22671976266108],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
     
+    {id:13, nombre:"La Bota Cervecera ", coordenadas: [-39.81289639386381, -73.24800577348306],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+        
+    {id:14, nombre:"Cervecería Mättig Spa", coordenadas: [-39.86179899527966, -73.19204416281889],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]},
+
+    {id:15, nombre:"Cervecería Elfo Del Bosque", coordenadas: [-39.842699789597006, -73.20635907804606],
+    tipos : [{tipo : "Lager", estado : 1}, {tipo : "Sinfiltar", estado : 1}, {tipo : "Bock", estado : 1}, {tipo : "PaleAle", estado : 1}, {tipo : "Trigo", estado : 1}]}
+
 ];
 
 export async function GET(){
