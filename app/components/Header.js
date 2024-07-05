a452ca891352228895d9a327f8df163a18dd2612
felipe pinuer <pinuer15@gmail.com>
2024-07-05 00:14:23 -0400

    arreglos esteticos

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,13 +1,17 @@
 export default function Header() {
   return (
     <div className="navegacion">
-        <img src="cerveza1.png" width="7%" height="7%"/> 
+      <img src="cerveza1.png" width="7%" height="7%"/>
+      <div className="header-content">
         <p className="titulo">Puntos de Cervecerías en Valdivia</p>
         <div className="leyenda">
           <ul>
             <li>Obtenga más información presionando los marcadores</li>
           </ul>
         </div>
-    </div>
+      </div>
+      <p className="creditos">
+        Desarrollado por Felipe Brana, Ninoska Toledo, Luis Pinuer y grupo 4 2° semestre 2023</p>
+      </div>
   );
 }
